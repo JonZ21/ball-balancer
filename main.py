24d18af439d6c0aa6f44f74dab2149eace828ccd
adnfletch <ajhfletc@uwaterloo.ca>
2025-11-10 13:01:51 -0500

    created main outline

---
+++ main.py
@@ -0,0 +1,20 @@
+import ball_tracking
+import camera_calibration
+from controller import  PIDcontroller
+
+#Placeholder to import the config code, obtain each unit vector for the axis
+#import the centroid location
+#setup for ball tracking as necessary
+
+#initialize PID controllers
+
+while(True):
+    
+    #Calculate the ball position
+
+    #Obtain and project the error onto each axis
+
+    #run each PID controller
+
+    #Send code to each motor
+
